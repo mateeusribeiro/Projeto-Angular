ad9433a161ea7d38c0454653de32262314c0e920
Mateus Ribeiro <mateeusribeiro>
2021-07-21 15:10:20 -0300

    Atualização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular11app';
+export class AppComponent implements OnInit {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+
+  ngOnInit(){
+    var myChart = new Chart("myChart", {
+      type: 'bar',
+      data: {
+          labels: ['Pernambuco', 'São Paulo', 'Rio de Janeiro', 'Rio Grande do Sul', 'Amazonas', 'Pará'],
+          datasets: [{
+              label: 'Estados',
+              data: [12, 19, 3, 5, 8, 3],
+              backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+              ],
+          }]
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TooltipModule, } from 'ngx-bootstrap/tooltip';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    TooltipModule.forRoot(),
+
     BrowserModule,
     AppRoutingModule
   ],
